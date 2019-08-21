e7c79f9dc3827044b668f804d376945ec35938f3
amaris chang <amaris.a.chang@gmail.com>
2019-08-21 15:56:27 -0400

    [feat]adds hpg modal-about

---
+++ client/components/aboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>ABOUT US</h1>
+      <p>
+        Two women navigate unemployment in the tech world, will they survive
+        with only a resume and the internet to guide them? Or will they be
+        destroyed by ravenous algorithm challenges that threaten to eat them
+        alive? Tune in as they interview champions of the job hunt and employed
+        software engineers with unconventional backgrounds willing to impart
+        wisdom, advice, and tricks of the trade.
+      </p>
+    </div>
+  )
+}
